318fdf201a6ddb7dc2d0dca54809125d9b7185ee
Mikael Tenhunen <mikael.tenhunen@gmail.com>
2021-06-09 20:12:35 +0200

    removes unused import

--- src/main/java/mikaeltenhunen/radioprograminfo/integration/EpisodeDeserializer.java
+++ src/main/java/mikaeltenhunen/radioprograminfo/integration/EpisodeDeserializer.java
@@ -3,7 +3,6 @@ package mikaeltenhunen.radioprograminfo.integration;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import mikaeltenhunen.radioprograminfo.domain.Episode;
 
